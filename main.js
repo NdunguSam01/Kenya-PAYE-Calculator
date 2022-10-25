98bc657b846e25027b9b3a6050231542c316e510
NdunguSam01 <ndungu.muigai01@gmail.com>
2022-10-25 11:22:49 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+//Input fields
+const period=document.getElementById('period');
+const basic=document.getElementById('basic');
+const benefits=document.getElementById('benefits');
+const nssfDed=document.getElementById('nssfDed');
+const nssfRate=document.getElementById('nssfRate');
+const nhifDed=document.getElementById('nhif');
+
+const calculate=()=>
+{
+    console.log(period.value);
+    console.log(basic);
+    console.log(benefits);
+    console.log(nssfDed.value);
+    console.log(nssfRate.value);
+    console.log(nhifDed.value);
+}
